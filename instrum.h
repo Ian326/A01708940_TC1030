a0800d576f03c657b29bcf103a71fefffc3b3da7
Ian Padrón Corona <a01708940@itesm.mx>
2021-11-11 19:23:37 -0600

    Added Racer class in library 'instrum.h'.

--- instrum.h
+++ instrum.h
@@ -1,6 +1,7 @@
 #include<string>
 
 class CarSports{
+
 private:
   std::string color;
   int model;
@@ -16,7 +17,6 @@ public:
    std::string get_color(){
      return color;
    };
-
    int get_model() {
      return model;
    };
@@ -32,12 +32,53 @@ public:
    float get_cylinders(){
      return cylinders;
    };
+
    void set_color(std::string );
    void set_model(int);
    void set_cylinders(int);
    void set_dim_x(float);
    void set_dim_y(float);
    void set_dim_z(float);
+};
+
+class RacerSports{
+
+private:
+  std::string name;
+  int age;
+  std::string sox;
+  float height;
+  float weight;
+  float imc;
+public:
+  RacerSports();
+  RacerSports(std::string name1, int age1, std::string sox1, float height1, float weight1):name(name1), age(age1), sox(sox1), height(height1), weight(weight1), imc(){};
+
+   std::string get_name(){
+     return name;
+   };
+   int get_age(){
+     return age;
+   };
+   std::string get_sox(){
+     return sox;
+   };
+   float get_height(){
+     return height;
+   };
+   float get_weight(){
+     return weight;
+   };
+   float get_imc(){
+     return weight/(height*height);
+   };
+
+   void set_name(std::string );
+   void set_age(int);
+   void set_sox(std::string);
+   void set_height(float);
+   void set_weight(float);
+
 };
 
 void CarSports::set_color(std::string color1){
@@ -57,4 +98,20 @@ void CarSports::set_dim_y(float dim_y1){
 }
 void CarSports::set_dim_z(float dim_z1){
   dim_z = dim_z1;
+}
+
+void RacerSports::set_name(std::string name1){
+  name = name1;
+}
+void RacerSports::set_age(int age1){
+  age = age1;
+}
+void RacerSports::set_sox(std::string sox1){
+  sox = sox1;
+}
+void RacerSports::set_height(float height1){
+  height = height1;
+}
+void RacerSports::set_weight(float weight1){
+  weight = weight1;
 }
