23b0b5db0b8ce890f8f93ee531306e63e2294402
Ian Padrón Corona <a01708940@itesm.mx>
2021-11-11 19:24:51 -0600

    Main now has 2 obj's (1 for each class)

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 int main() {
-  std::cout << "Hola mundo!"<<endl;
-  std::cout << "Soy yo, Ian :)"<<endl;
+  std::cout << "Hola mundo :D!\n"<<endl;
+  RacerSports racer_1("Ian", 18, "Masculine", 1.72, 55);
+  std::cout <<"Bienvenido, " <<racer_1.get_name() <<"!\n";
 
   CarSports car_1("Blue", 2018, 3.72, 1.65, 1.20, 6);
-  std::cout <<"El numero de Cilindros del Carro es de: " <<car_1.get_cylinders();
+  std::cout <<"El numero de Cilindros de tu auto es de: " <<car_1.get_cylinders();
+
 }
